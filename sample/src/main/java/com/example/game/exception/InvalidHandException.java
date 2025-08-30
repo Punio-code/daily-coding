b97aeb8a65ee9b0d1811926bd3184de3360bbc47
junji <test@test.com>
2025-08-31 02:04:36 +0900

    じゃんけんゲームを実装。

---
+++ sample/src/main/java/com/example/game/exception/InvalidHandException.java
@@ -0,0 +1,16 @@
+package com.example.game.exception;
+
+/**
+ * 無効な手が入力された場合の例外クラスです。ユーザーが不正な値を入力した際にスローされます。
+ */
+public class InvalidHandException extends Exception {
+
+  /**
+   * 指定されたメッセージでInvalidHandExceptionを構築します。
+   *
+   * @param message 例外メッセージ
+   */
+  public InvalidHandException(String message) {
+    super(message);
+  }
+}
