484e987f0a66427613346810708e9c2333f9aecd
Punio <duzhishanye68@gmail.com>
2026-01-13 01:21:19 +0900

    オーバーロード仕様は以下： 同一メソッド名であること 引数リスト（型・個数・順序）が異なれば成立 戻り値の型は関係ない アクセス修飾子は関係ない 呼び出し解決はコンパイル時 オーバーライドとは別概念

---
+++ practice/src/TestOverload.java
@@ -0,0 +1,14 @@
+public class TestOverload {
+
+    public void testoverload() {
+
+    }
+
+    public void testoverload(String message) {
+
+    }
+
+    public void testoverload(String message,int count) {
+
+    }
+}
